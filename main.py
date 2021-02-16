b1a4cd8e689e1154368114327ec87e1fbd8f8796
Aattawut Nutlamyong <Aattawut.field@gmail.com>
2021-02-16 17:43:35 +0700

    okkk

--- main.py
+++ main.py
@@ -4,14 +4,5 @@ app = FastAPI()
 
 
 @app.get("/")
-async def helloworld():
+def helloworld():
     return 'Hello World FastAPI'
-
-
-@app.get("/bmi")
-async def bmi(weight: float, height: float):
-    '''
-    คำนวณค่าดัชนีมวลกาย (bmi) จาก น้ำหนัก(kg)/ส่วนสูง(m)**2"
-    '''
-    bmi = weight/(height/100)**2
-    return str(bmi)
